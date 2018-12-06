bca211aa6431b1d9cf1ca768a92b6b304f649de3
niuzl <niuzl@feimarobotics.com>
2018-12-06 17:38:34 +0800

    增加拍照功能

--- template/d200_driver/radio/radio.c
+++ template/d200_driver/radio/radio.c
@@ -15,10 +15,13 @@
 /* linker settings */
 static unsigned char radio_settings;
 /*--------------------------*/
-struct file * file_log;
+struct file * file_log,*gim;
 static unsigned int cnt = 0;
+static unsigned int delay_time = 0;
+static unsigned int pic_cnt = 0;
+void pic_thread(void);
 /* fs inode system register */
-FS_INODE_REGISTER("/test.o",radio,radio_heap_init,0);
+FS_INODE_REGISTER("/pic_test.o",radio,radio_heap_init,0);
 /* fs shell def */
 FS_SHELL_REGISTER(radio_settings);
 /* heap init */
@@ -32,17 +35,28 @@ int radio_heap_init(void)
 	radio.config = radio_default_config;
 	/* file interface */
 	radio.flip.f_inode = &radio;
-	radio.flip.f_path = "/test.o";
+	radio.flip.f_path = "/pic_test.o";
 	/* setting init */
 	FS_SHELL_INIT(radio_settings,radio_settings,0x010000 + 1 , _CB_VAR_);
 	/* clear */
 	radio_settings = 0;
 	file_log = 0;
 	cnt = 0;
+	gim = 0;
+	delay_time = 240;
+	pic_cnt = 0;
 	/* add your own code here */
   radio.i_flags = __FS_IS_INODE_OK|__FS_IS_INODE_INIT;
 	/* ------end of file------ */
 	return FS_OK;
+}
+/*- printf to file */
+void printf_f(const char * p)
+{
+	if( file_log != 0 )
+	{
+		fs_write(file_log,p,strlen(p));
+	}
 }
 /* default config */
 int radio_default_config(void)
@@ -52,39 +66,68 @@ int radio_default_config(void)
 	char * p_path = (char *)shell_find("log_num_exprot")->enter;
 	/*-----------------------------*/
 	/* merge path */
-	sprintf(buffer,"/storage/emmc/shell/ok_%s.txt",p_path);
+	sprintf(buffer,"/storage/emmc/shell/take_pictures_%s.txt",p_path);
 	/* open new file */
 	file_log = open(buffer,__FS_CREATE_ALWAYS|__FS_WRITE);	
 	/* create */
-	if( file_log != 0 )
+	if( file_log == 0 )
 	{
-		fs_write(file_log,"file create ok\r\n",strlen("file create ok\r\n"));
-		
-		if( fs_ioctl(file_log,0,0,(void *)"/storage/backup/package/D200_Autopilot_Backup.fm") == FS_OK )
-		{
-			fs_write(file_log,"backup.fm delete has been ok\r\n",strlen("backup.fm delete has been ok\r\n"));
-		}else
-		{
-			fs_write(file_log,"backup.fm delete has been failed\r\n",strlen("backup.fm delete has been failed\r\n"));
-		}
-			
-		
-//		 shell_create_dynamic("radio_thread",radio_thread,5);//500ms /storage/backup/package/D200_Autopilot_Backup.fm
+		printf_f("file create FAIL\r\n");
+		return FS_ERR;
 	}
+	/* create ok */
+	printf_f("file create OK \r\n");
+	/* open gimbal */
+	gim = open("/gimbal.d",__FS_OPEN_ALWAYS);
+	/* judging */
+	if( gim == 0 )
+	{
+		printf_f("gimbal open FAIL.break\r\n");
+		return FS_ERR;		
+	}
+	printf_f("gimbal open OK\r\n");
+	/*------------------------*/
+	if( shell_create_dynamic("pic_thread",pic_thread,5) != FS_OK )
+	{
+		printf_f("pic_thread create FAIL\r\n");
+		return FS_ERR;		
+	}
+	/*-----------*/
+	printf_f("pic_thread create OK\r\n");
+	printf_f("taking...\r\n");
 	/* return */
 	return FS_OK;
 }
-///* thread */
-//void radio_thread(void)
-//{
-//	char buffer[8];
-//  /* clear */
-//	memset(buffer,0,sizeof(buffer));
-//	/*----------------------------*/
-//	sprintf(buffer,"%d\r\n",cnt++);
-//	/* das */
-//	fs_write(file_log,buffer,strlen(buffer));
-//}
+
+/* thread */
+void pic_thread(void)
+{
+	/*---------*/
+	if( delay_time > 0 )
+	{
+		delay_time -- ;
+		return;
+	}
+	/*------------*/
+	if( cnt >= 4 )//2s
+	{
+		cnt = 0;
+		/* take pic */
+		fs_ioctl(gim,2,1,0);
+		/* inc */
+		pic_cnt ++;
+		/*-----*/
+		char buffer[32];
+		/* clear */
+		memset(buffer,0,sizeof(buffer));
+		/*----------------------------*/
+		sprintf(buffer,"PIC:%d\r\n",pic_cnt++);
+		/* das */
+		printf_f(buffer);		
+	}
+	/* incremeters */
+	cnt++;
+}
 
 
 
